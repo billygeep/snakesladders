6d8c98de783b1b4dfba8b2c838150c0a62e9c28b
Josh King <josh.king@ralphandco.com>
2018-03-20 23:23:54 +0000

    added objects

--- game/js/states/Game.js
+++ game/js/states/Game.js
@@ -6,12 +6,49 @@ var Game = function(game) {};
 var gameview
 
 
-var gameScene, wallSprite_01, wallSprite_02, wallSprite_03, wall_x = 0;
+var gameScene, wallSprite_01, wallSprite_02, ladder_01, snake_01, wallSprite_03, wall_x = 0;
+
+var current_floor = 0;
+var range = 4;
+var jump = false;
+var drop = false;
+var floor_transition = 0;
+var start_y = 300;
+
+var wall_width = 168, wall_height = 89;
 
 var wall_array = [
-  [ 0, 1, 0, 2, 1 ],
-  [ 2, 0, 0, 0, 2 ],
-  [ 1, 2, 1, 1, 1 ]
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 2, 2, 2, 2, 2, 2, 2, 2 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 2, 0, 0, 0, 2, 2, 0, 2 ],
+  [ 0, 1, 0, 2, 1, 1, 1, 1 ],
+  [ 2, 0, 0, 0, 2, 1, 1, 1 ],
+  [ 1, 2, 1, 1, 1, 2, 2, 2 ],
+  [ 0, 1, 0, 2, 1, 0, 2, 1 ],
+  [ 2, 2, 2, 2, 2, 2, 2, 2 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 2, 0, 0, 0, 2, 2, 0, 2 ],
+  [ 0, 1, 0, 2, 1, 1, 1, 1 ],
+  [ 2, 0, 0, 0, 2, 1, 1, 1 ],
+  [ 1, 2, 1, 1, 1, 2, 2, 2 ]
+]
+
+var object_array = [
+  [ 0, 0, 1, 0, 0, 0, 0, 0 ],
+  [ 0, 1, 0, 2, 0, 0, 0, 0 ],
+  [ 0, 0, 1, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 1, 0, 2, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 2, 0, 0, 0 ],
+  [ 0, 1, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 2, 0, 0, 1, 0, 0 ],
+  [ 0, 1, 0, 0, 0, 0, 0, 0 ]
 ]
 
 
@@ -20,6 +57,12 @@ Game.prototype = {
 
   init: function () {
     gameview = this;
+
+    this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    this.upKey.onDown.add(this.jumpFloor, this);
+
+    this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    this.downKey.onDown.add(this.dropFloor, this);    
   },
 
   ////////////////////////////////////
@@ -48,6 +91,9 @@ Game.prototype = {
     wallSprite_02 = game.make.sprite(0, 0, 'furniture_sprites', 'quit_button.png');
     wallSprite_03 = game.make.sprite(0, 0, 'furniture_sprites', 'quit_button_over.png');
 
+    ladder_01 = game.make.sprite(0, 0, 'furniture_sprites', 'close_up.png');
+    snake_01 = game.make.sprite(0, 0, 'furniture_sprites', 'close_over.png');
+
     this.createLevels();
     this.createHero();
   },
@@ -65,7 +111,7 @@ Game.prototype = {
   },
 
   createHero: function () {
-     this.hero = game.make.sprite(0, 0, 'furniture_sprites', 'pause_button.png');
+     this.hero = new Hero(game, 0, 0);
   },
 
   ////////////////////////////////////
@@ -76,19 +122,27 @@ Game.prototype = {
   ////////////////////////////////////
   ////////////////////////////////////
 
+  jumpFloor: function () {
+    jump = true;
+  },
+
+  dropFloor: function () {
+    drop = true;
+  },
+
   update: function () {
     this.renderScene();
+    if (jump) this.transitionFloor(2);
+    if (drop) this.transitionFloor(-2);
+
   },
 
   renderScene: function () {
+
     gameScene.clear();
 
-    // var startTileX=Math.max(0,0-cornerMapTile.x);
-    // var startTileY=Math.max(0,0-cornerMapTile.y);
-    // var endTileX=Math.min(this.levelData[0].length,startTileX+visibleTiles.x);
-    // var endTileY=Math.min(this.levelData.length,startTileY+visibleTiles.y);
-
-    wall_x -= 2;
+    wall_x -= 4;
+    wall_y = start_y + (current_floor*wall_height) + floor_transition
 
     var shift_element = false;
 
@@ -96,11 +150,14 @@ Game.prototype = {
       shift_element = true;
     }
 
-
-    for (var i = 0; i < wall_array.length; i++) {
+    for (var i = current_floor; i < current_floor+range; i++) {
       for (var j = 0; j < wall_array[i].length; j++) {
         var tile = this.getTile(wall_array[i][j])
         this.drawTile(tile, j, i);
+        if(object_array[i][j] > 0) {  
+          var object = this.getObject(object_array[i][j]);
+          this.drawObject(object, j, i);
+        }
       }
     }
 
@@ -108,20 +165,50 @@ Game.prototype = {
 
     if (shift_element) {
       wall_x = 0;
-      for (var k = 0; k < wall_array.length; k++) {
+      for (var k = current_floor; k < current_floor+range; k++) {
         wall_array[k].shift();
         wall_array[k].push(this.getNewTile(k));
+
+        object_array[k].shift();
+        object_array[k].push(this.getNewTile(k));
       }
+    }  
+  },
+
+  transitionFloor: function (_val) {
+    floor_transition += _val;
+
+    if (floor_transition >= wall_height) {
+      current_floor++;
+      floor_transition = 0;
+      jump = false;
     }
-    
+    if (floor_transition <= -wall_height) {
+      current_floor--;
+      floor_transition = 0;
+      drop = false;
+    }
   },
 
   drawHero: function() {
-    gameScene.renderXY(this.hero, 100, 65, false);
+    gameScene.renderXY(this.hero, 300, start_y+50, false);
   },
 
   drawTile: function(tile, x, y) {
-    gameScene.renderXY(tile, (x*tile.width)+wall_x, y*-tile.height, false);
+    gameScene.renderXY(tile, (x*wall_width)+wall_x, wall_y - (y*wall_height), false);
+  },
+
+  drawObject: function(object, x, y) {
+
+    var myX = (x*wall_width)+wall_x;
+    var myY = wall_y - (y*wall_height);
+
+    gameScene.renderXY(object, myX, myY, false);
+    
+    if (this.hero.getBounds().contains(myX, myY)) {
+    //   console.log('GO')
+    console.log('GO')
+    }
   },
 
   getTile: function (_tile) {
@@ -143,9 +230,27 @@ Game.prototype = {
     return tile;
   },
 
-  getNewTile: function (_level) {
-    var tile = Math.floor(Math.random()*_level);
+  getObject: function (_object) {
 
+    var object;
+
+    switch (_object) {
+      case 0 :
+     
+      break;
+      case 1 :
+        object = ladder_01;
+      break;
+      case 2 :
+        object = snake_01;
+      break;
+    }
+    return object;
+  },
+
+  // get the new background tile
+  getNewTile: function (_level) {
+    var tile = Math.floor(Math.random()*3);
     return tile;
   },
 
